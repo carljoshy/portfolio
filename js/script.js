b6af315cd4711007730255e8c529adf9411eff2d
carljoshy <carljoshy12@gmail.com>
2025-09-06 13:03:57 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Change navbar background on scroll
+window.addEventListener("scroll", function() {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+  
+  // Smooth scroll for anchor links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
+
+
+  
+  
